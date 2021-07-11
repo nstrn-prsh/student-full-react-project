e4d6a8c00d40cf2041842de4658de0a7e26a4999
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-07-11 22:18:06 +0430

    handle error

--- src/components/students/Students.jsx
+++ src/components/students/Students.jsx
@@ -3,19 +3,21 @@ import Student from "./student/Student";
 import PropTypes from "prop-types";
 import { memo } from "react";
 import "./student/student.css";
+import ErrorBoundary from "../ErrorBoundaries/ErrorBoundary";
 
 const Students = (props) => {
      let students = props.studentsList.map((student, index) => (
-          <Student
-               key={index}
-               id={student.id}
-               name={student.name}
-               classNumber={student.classNumber}
-               phoneNumber={student.phoneNumber}
-               email={student.email}
-               deleted={props.deleted}
-               nameChanged={(event) => props.nameChange(event, student.id)}
-          />
+          <ErrorBoundary key={index}>
+               <Student
+                    id={student.id}
+                    name={student.name}
+                    classNumber={student.classNumber}
+                    phoneNumber={student.phoneNumber}
+                    email={student.email}
+                    deleted={props.deleted}
+                    nameChanged={(event) => props.nameChange(event, student.id)}
+               />
+          </ErrorBoundary>
      ));
 
      if (props.toggle) {
